95cef3e6ecb5f3d59ac2af53eb97e8b165d619fd
Оstrovaya Еlena <elena.ostrowaya@yandex.ru>
2023-06-16 00:11:07 +0300

    задание6

---
+++ задание6/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+Console.Write("Введите число:");
+string userInput1 = Console.ReadLine() ?? "";
+int a = int.Parse(userInput1);
+
+if (a % 2 == 0)
+{
+    Console.WriteLine("Число четное");
+}
+else
+{
+    Console.WriteLine("Число нечетное");
+}
